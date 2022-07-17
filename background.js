9fa1417aa96d3e8be17f98cf0c6ce57e822be590
yo <colacat1927@gmail.com>
2022-07-17 16:08:48 +0300

    добавил скрытие popup

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("installed");
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Additionally, you can check for changeInfo.status
+  if (/^https:\/\/s\.taobao.com/.test(tab.url)) {
+    chrome.action.setPopup({
+      tabId: tab.id, // Set the new popup for this tab.
+      popup: "popup.html", // Open this html file within the popup.
+    });
+  }
+});
