3e9d42ada5986ae67a727abbf824f29d43198675
yo <colacat1927@gmail.com>
2022-07-18 09:59:04 +0300

    change popup to options page

--- background.js
+++ background.js
@@ -2,12 +2,19 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("installed");
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // Additionally, you can check for changeInfo.status
-  if (/^https:\/\/s\.taobao.com/.test(tab.url)) {
-    chrome.action.setPopup({
-      tabId: tab.id, // Set the new popup for this tab.
-      popup: "popup.html", // Open this html file within the popup.
-    });
-  }
+chrome.action.onClicked.addListener(function (tab) {
+  console.log("icon clicked");
+  chrome.tabs.create({
+    url: "options.html",
+  });
 });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   // Additionally, you can check for changeInfo.status
+//   // if (/^https:\/\/s\.taobao.com/.test(tab.url)) {
+//   //   chrome.action.setPopup({
+//   //     tabId: tab.id, // Set the new popup for this tab.
+//   //     popup: "popup.html", // Open this html file within the popup.
+//   //   });
+//   // }
+// });
