cfd5f702f3a767cd026e2e1e53ec71b7f2d25301
yo <colacat1927@gmail.com>
2022-07-23 23:11:31 +0300

    create modal

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,7 +1,42 @@
 async function scraper() {
+  //Додаємо на сторінку повідомлення про стан збереження в таблицю
+  addCSS("assets/alert/alert.css");
+
   //зберігаємо таймаут
   let timeout = await getStorageData("timeout");
   timeout = parseInt(timeout);
+
+  const body = document.querySelector("body");
+
+  const modalContainer = document.createElement("div");
+  modalContainer.classList.add("modalContainer");
+
+  const modal = document.createElement("div");
+  modal.classList.add("sheetsContent");
+  modal.id = "sheetsPopup";
+
+  const close = document.createElement("button");
+  close.classList.add("close");
+  close.textContent = "✖";
+  modal.append(close);
+
+  const img = document.createElement("img");
+  img.src = "https://cdn-icons-png.flaticon.com/512/281/281761.png";
+  modal.append(img);
+
+  const p = document.createElement("p");
+  p.textContent = "Парсинг завершено, всі дані збережено до таблиці";
+  modal.append(p);
+
+  const button = document.createElement("button");
+  button.classList.add("accept");
+  button.textContent = "Продовжити";
+
+  modalContainer.append(modal);
+  modal.append(button);
+
+  body.append(modalContainer);
+  body.style.overflow = "hidden";
 
   //зберігаємо shopId
   const shopId = document.querySelector("div[shopId]").getAttribute("shopId");
